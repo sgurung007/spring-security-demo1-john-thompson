ae092eb1a5cf85b79a98282ba2030b9e49c0cec9
bir <sgrg007>
2021-12-21 17:09:04 -0600

    clean up

--- src/main/java/com/gurung/springsecuritydemo1/config/SecurityConfig.java
+++ src/main/java/com/gurung/springsecuritydemo1/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.gurung.springsecuritydemo1.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,10 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author bir
